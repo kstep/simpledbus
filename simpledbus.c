kstep/simpledbus

commit 8a2106ea9de1968d0dac4ceec01716621da188d8

call methods asyncronously only if main loop is running

simpledbus.c
@@ -335,7 +335,8 @@ static int bus_call_method(lua_State *L)
 			add_arguments(L, 7, lua_gettop(L), signature, msg);
 	}
 
-	if (!lua_pushthread(L)) { /* L can be yielded */
+	/* if (!lua_pushthread(L)) { / * L can be yielded */
+	if (mainThread) { /* main loop is running */
 		DBusPendingCall *pending;
 
 		if (!dbus_connection_send_with_reply(c->conn, msg, &pending, -1))
@@ -414,10 +415,12 @@ static DBusHandlerResult signal_handler(DBusConnection *conn,
 		return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
 	}
 
-	/* create new Lua thread and move the
-	 * Lua signal handler there */
+	/* create new Lua thread */
 	T = lua_newthread(mainThread);
+	/* push nil to let whoever sees the end of this thread
+	 * know that nothing further needs to be done */
 	lua_pushnil(T);
+	/* move the Lua signal handler there */
 	lua_insert(mainThread, -2);
 	lua_xmove(mainThread, T, 1);
 

commit 73b7dbffedbc50e50163c46c6e5bc9fc8da0b6b0

fix minor bug introduced in 8a2106ea9de1968d0dac4ceec01716621da188d8

simpledbus.c
@@ -351,7 +351,7 @@ static int bus_call_method(lua_State *L)
 		lua_settop(L, 1);
 		return lua_yield(L, 1);
 	}
-	lua_pop(L, 1);
+	/* lua_pop(L, 1); */
 
 	/* L is the main thread, so we call the method synchronously */
 	ret = dbus_connection_send_with_reply_and_block(c->conn, msg, -1, &err);