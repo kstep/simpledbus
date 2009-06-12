a01d7b5f2e1d7da9f10067e9b8023c2310636a9a
Emil Renner Berthing <esmil@mailme.dk>
2009-06-12 05:39:26 +0200

    license under LGPL instead of GPL

--- parse.h
+++ parse.h
@@ -1,3 +1,21 @@
+/*
+ * SimpleDBus - Simple DBus bindings for Lua
+ * Copyright (C) 2008 Emil Renner Berthing <esmil@mailme.dk>
+ *
+ * SimpleDBus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimpleDBus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SimpleDBus. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _PARSE_H
 #define _PARSE_H
 
--- simpledbus.lua
+++ simpledbus.lua
@@ -3,8 +3,8 @@
    Copyright (C) 2008 Emil Renner Berthing <esmil@mailme.dk>
 
    SimpleDBus is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
+   it under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
    SimpleDBus is distributed in the hope that it will be useful,
@@ -12,7 +12,7 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with SimpleDBus. If not, see <http://www.gnu.org/licenses/>.
 --]]
 
